9e4aaa86b7af89604cc53a5640b7269381920f3e
anjanasilva99 <anjana@shiftgroup.ca>
2025-04-28 21:51:00 +0530

    Add SiteIntro component with introductory text and responsive styles

---
+++ src/components/SiteIntro.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="site-intro">
+    <div class="container">
+      <h1>MOVIE LIBRARY</h1>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SiteIntro'
+}
+</script>
+
+<style scoped>
+.site-intro {
+  padding: 60px 0;
+  text-align: center;
+  background-color: #f0f0f0;
+}
+
+.site-intro h1 {
+  font-size: 36px;
+  margin-bottom: 20px;
+  text-transform: uppercase;
+}
+
+.site-intro p {
+  max-width: 800px;
+  margin: 0 auto;
+  font-size: 18px;
+}
+
+@media (max-width: 768px) {
+  .site-intro {
+    padding: 40px 0;
+  }
+  
+  .site-intro h1 {
+    font-size: 28px;
+  }
+  
+  .site-intro p {
+    font-size: 16px;
+  }
+}
+</style>
